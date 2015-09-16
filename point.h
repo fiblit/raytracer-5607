f4b2d04932d38f1e01d448ed59d0ff5a398c5522
Dalton Hildreth <hildr039@umn.edu>
2015-09-16 00:50:48 -0500

    Added many of the classes specificied by TODO. All of their functionality is still to be implemented.

---
+++ point.h
@@ -0,0 +1,12 @@
+#ifndef POINT_H
+#define POINT_H
+
+class point
+{
+    public:
+        point();
+    protected:
+    private:
+};
+
+#endif // POINT_H
---
+++ sphere.h
@@ -0,0 +1,13 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+
+class sphere
+{
+    public:
+        sphere();
+    protected:
+    private:
+};
+
+#endif // SPHERE_H
