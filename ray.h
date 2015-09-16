d8ad0319934587cb2f40615f51765565e1df0603
Dalton Hildreth <hildr039@umn.edu>
2015-09-16 00:50:48 -0500

    Added many of the classes specificied by TODO. All of their functionality is still to be implemented.

---
+++ ray.h
@@ -0,0 +1,13 @@
+#ifndef RAY_H
+#define RAY_H
+
+
+class ray
+{
+    public:
+        ray();
+    protected:
+    private:
+};
+
+#endif // RAY_H
---
+++ rgb.h
@@ -0,0 +1,13 @@
+#ifndef RGB_H
+#define RGB_H
+
+
+class rgb
+{
+    public:
+        rgb();
+    protected:
+    private:
+};
+
+#endif // RGB_H
