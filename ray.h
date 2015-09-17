99eb6d88f5c526a20ac4d708f04839e818135d3e
Dalton Hildreth <hildr039@umn.edu>
2015-09-17 02:56:04 -0500

    Did some prototyping.

--- ray.h
+++ ray.h
@@ -6,8 +6,13 @@ class ray
 {
     public:
         ray();
+        ray(point origin, vector3 dir);
+        ray(double x0, double y0, double z0, double xd, double yd, double zd);
+
     protected:
     private:
+        point origin;
+        vector3 dir;
 };
 
 #endif // RAY_H
